37dd3630e2165ae0e021e8fe04eb69bc3e6e297a
dhananjaythomble <dhananjaythomble@gmail.com>
2023-01-13 17:35:20 +0530

    add basic index page

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [url, setUrl] = useState('');
+    const [error, setError] = useState('');
+    const [shortUrl, setShortUrl] = useState('');
+    const handleSubmit = () => {
+
+    }
+
+    return (
+        <div>
+            <Header/>
+
+            <main>
+                <section>
+                    <form id="shorten-form">
+                        <label htmlFor="url">Enter the URL to Shorten:</label>
+                        <input type="url" id="url" name="url" required/>
+                        <button type="submit">Shorten</button>
+                    </form>
+                </section>
+
+                <section id="shortened-urls">
+                    <h2>Recent Shortened URLs</h2>
+                    <table>
+                        <thead>
+                        <tr>
+                            <th>Short URL</th>
+                            <th>Original URL</th>
+                            <th>Actions</th>
+                        </tr>
+                        </thead>
+                        <tbody id="url-table-body"></tbody>
+                    </table>
+                </section>
+            </main>
+            <Footer/>
+        </div>
+    );
+
 }
+
+function Header() {
+    return (
+        <header>
+            <h1>URL Shortener</h1>
+        </header>
+    );
+}
+
+const Footer = () => (
+    <footer>
+        <p>By <a href="https://dhananjaythomble.me/">Dhananjay Thomble</a></p>
+    </footer>
+)
 
 export default App;
