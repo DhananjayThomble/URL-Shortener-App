8d26090d598a1f87c90f3dbc1a252e3c41276886
dhananjaythomble <dhananjaythomble@gmail.com>
2023-01-22 17:37:56 +0530

    add toast and fetch in home component.

--- react-frontend/src/components/Home.js
+++ react-frontend/src/components/Home.js
@@ -1,16 +1,57 @@
 import React, { useState } from "react";
 import { Container, Row, Col, Form, Button } from "react-bootstrap";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.min.css";
 
 function Home() {
   const [url, setUrl] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    // show toast
+    toast("URL will be shortened soon!");
     console.log("url value is ", url);
+
+    fetch("https://app.dhananjaythomble.me/api", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ url: url }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        // if (data.status === 200) {
+        //   setUrl("");
+        //   console.log(data);
+        // } else {
+        //   alert(data.message);
+        //   console.log(data);
+        // }
+        if (data) {
+          setUrl(data.short_url);
+          console.log(data.short_url);
+        }
+      })
+      .catch((error) => {
+        alert(error);
+      });
   };
 
   return (
     <>
+      <ToastContainer
+        position="top-center"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
       <div className={"mt-4 p-4 bg-primary text-white rounded jumbotron"}>
         <Container>
           <h1>Welcome to URL Shortener</h1>
@@ -30,6 +71,7 @@ function Home() {
                 <Form.Control
                   type="url"
                   placeholder="https://example.com"
+                  value={url}
                   onChange={(e) => setUrl(e.target.value)}
                 />
               </Form.Group>
