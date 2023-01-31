9d0a6ca3f46174f64305bc49b4d9fe903eca2348
dhananjaythomble <dhananjaythomble@gmail.com>
2023-01-31 14:42:28 +0530

    misc

--- routes/apiAuth.js
+++ routes/apiAuth.js
@@ -12,14 +12,14 @@ const router = express.Router();
 export function isApiAuthenticated(req, res, next) {
   //  check if token is present in headers
   if (!req.headers.authorization) {
-    res.status(401).json({ message: "Unauthorized" });
+    res.status(401).json({ error: "Unauthorized" });
     return;
   }
   // verify the token
   const token = req.headers.authorization.split(" ")[1];
   jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
     if (err) {
-      res.status(401).json({ message: "Invalid token" });
+      res.status(401).json({ error: "Invalid token" });
     } else {
       req.user = decoded;
       next();
