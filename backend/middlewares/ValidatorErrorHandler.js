7e6ea446d9550bef39a36e784cc52afdd8665a83
Abhishek Gawali <gawalibhshk@gmail.com>
2023-10-04 14:30:12 +0530

    resolve #15

---
+++ backend/middlewares/ValidatorErrorHandler.js
@@ -0,0 +1,12 @@
+import { validationResult } from 'express-validator';
+
+
+export const validationErrorHandler = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  next();
+};
+
+
--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,15 +1,16 @@
 import Router from "express";
 
 import {
-  validateLogin,
-  validateSignup,
   login,
   signup,
 } from "../controllers/auth.controller.js";
+import { validateLogin, validateSignup } from '../validators/AuthValidators.js';
+import { validationErrorHandler } from '../middlewares/ValidatorErrorHandler.js';
+
 
 const router = Router();
-router.post("/login", validateLogin, login);
+router.post("/login", validateLogin, validationErrorHandler, login);
 
-router.post("/signup", validateSignup, signup);
+router.post("/signup", validateSignup,validationErrorHandler, signup);
 
 export default router;
