3092383dda351c07722ee0c79a8880534e18de53
Nikhil Babhulkar <nikhilbabhulkar2019@gmail.com>
2023-10-05 12:54:26 +0530

    fix the routes

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -14,7 +14,7 @@ import { validationErrorHandler } from '../middlewares/ValidatorErrorHandler.js'
 const router = Router();
 router.post("/login", validateLogin, validationErrorHandler, login);
 router.post("/signup", validateSignup,validationErrorHandler, signup);
-router.post("/forgot-password",forgotPassword);  //For Sending the password reset request
+router.post("/forget-password",forgotPassword);  //For Sending the password reset request
 router.post("/reset-password",resetPassword);   // For Reseting the password
 router.get("/verify-email", verifyEmail);
 
