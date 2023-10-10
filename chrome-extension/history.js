4b250a8777223229438d05eca5bc1bc4f75410c2
Abhijeet <abhijeetcode05@gmail.com>
2023-10-10 09:20:07 +0530

    issue #32

---
+++ chrome-extension/history.js
@@ -0,0 +1,51 @@
+const token =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTFjZDJiNTI1YTUwOGIwODQ0NmQzYWIiLCJpYXQiOjE2OTYzODc3ODQsImV4cCI6MTY5Njk5MjU4NH0.j1ubpVSrqfm9DhTUYQ4xBhZJj1WYHY1E5WmAYX4HnBM";
+
+document.addEventListener("DOMContentLoaded", async function () {
+  const historyTable = document.getElementById("historyTable");
+
+  try {
+    // Fetch history data from your server
+    const response = await fetch("https://dturl.live/api/history", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`, // Replace with your actual authorization token
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error("Error fetching history data.");
+    }
+
+    const historyData = await response.json();
+    console.log(historyData.urlArray);
+    // Function to populate the table with history data
+    function populateTable() {
+      historyData.urlArray.forEach((entry) => {
+        const row = document.createElement("tr");
+
+        const shortUrlCell = document.createElement("td");
+        shortUrlCell.textContent = entry.shortUrl;
+
+        const originalUrlCell = document.createElement("td");
+        originalUrlCell.textContent = entry.originalUrl;
+
+        const visitCountCell = document.createElement("td");
+        visitCountCell.textContent = entry.visitCount;
+
+        row.appendChild(shortUrlCell);
+        row.appendChild(originalUrlCell);
+        row.appendChild(visitCountCell);
+
+        historyTable.appendChild(row);
+      });
+    }
+
+    // Call the populateTable function when the page loads
+    populateTable();
+  } catch (error) {
+    console.error(error);
+    alert("An error occurred while fetching history data.");
+  }
+});
--- chrome-extension/script.js
+++ chrome-extension/script.js
@@ -1,38 +1,82 @@
-const form = document.querySelector("form");
-form.addEventListener("submit", async (e) => {
-  e.preventDefault();
-  const url = form.url.value;
-  const data = { url };
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.querySelector("form");
+  const urlInput = form.querySelector("#inputUrl");
+  const shortUrl = form.querySelector("#shortUrl");
+  const copyBtn = form.querySelector("#copyBtn");
+  const token =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTFjZDJiNTI1YTUwOGIwODQ0NmQzYWIiLCJpYXQiOjE2OTYzODc3ODQsImV4cCI6MTY5Njk5MjU4NH0.j1ubpVSrqfm9DhTUYQ4xBhZJj1WYHY1E5WmAYX4HnBM";
+  form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    const longUrl = urlInput.value.trim(); // Trim any leading/trailing spaces
 
-  const response = await fetch("https://app.dhananjaythomble.me/api", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(data),
+    if (!longUrl) {
+      // Check if the URL input is empty
+      alert("Please enter a valid URL.");
+      return;
+    }
+
+    const data = { url: longUrl };
+
+    try {
+      const response = await fetch("https://dturl.live/api/url", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify(data),
+      });
+
+      if (!response.ok) {
+        throw new Error("Error shortening URL.");
+      }
+
+      const responseData = await response.json();
+
+      shortUrl.value = responseData.shortUrl;
+      shortUrl.style.display = "block";
+      copyBtn.style.display = "block";
+    } catch (error) {
+      console.error(error);
+      alert("An error occurred while shortening the URL.");
+    }
   });
 
-  const responseData = await response.json();
-  // console.log(responseData);
-
-  const shortUrl = document.querySelector("#shortUrl");
-  shortUrl.value = responseData.short_url;
-  // display style of shortUrl
-  shortUrl.style.display = "block";
-
-  const copyBtn = document.querySelector("#copyBtn");
-  copyBtn.style.display = "block";
+  // Copy to clipboard functionality (no changes needed)
   copyBtn.addEventListener("click", () => {
-    const shortUrl = document.querySelector("#shortUrl");
     shortUrl.select();
     document.execCommand("copy");
     copyBtn.innerHTML = "Copied to clipboard";
   });
-});
 
-// get current tab url
-chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-  const url = tabs[0].url;
-  const form = document.querySelector("form");
-  form.url.value = url;
+  // Get current tab URL
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    const url = tabs[0].url;
+    urlInput.value = url;
+  });
+
+  // Add a click event listener to the historyBtn
+  historyBtn.addEventListener("click", async () => {
+    // try {
+    //   const response = await fetch("https://dturl.live/api/history", {
+    //     method: "GET",
+    //     headers: {
+    //       "Content-Type": 'application/json',
+    //       Authorization: `Bearer ${token}`,
+    //     },
+    //   });
+
+    //   if (!response.ok) {
+    //     throw new Error("Error fetching history.");
+    //   }
+
+    //   const responseData = await response.json();
+    //   console.log(responseData)
+    //   // Open the history in a new tab
+      chrome.tabs.create({ url: chrome.runtime.getURL('history.html')});
+    // } catch (error) {
+    //   console.error(error);
+    //   alert("An error occurred while fetching the history.");
+    // }
+  });
 });
